71beea7451188422aeb6f2f50cde3954ad15f432
Kanukolu <104075953+Kanukolu@users.noreply.github.com>
2023-10-13 11:24:23 +0530

    auth

---
+++ expensetracker/backend/controller/user.js
@@ -0,0 +1,52 @@
+// const Expense = require('../models/user')
+// const bcrypt = require('bcrypt');
+// exports.createuser=async (req,res)=>{
+//     try{
+
+    
+//     const name = req.body.name;
+//     const email = req.body.email;
+//     const password = req.body.password;
+//     let result = await User.findOne({where : {email : email}})
+//     console.log(result)
+//     if(result !== null)
+//         return res.status(401).json({success : false , msg : "User already exists"})
+//     let hash = await bcrypt.hash(password , 10);
+//     const user = await User.create({name : name , email : email , password : hash})
+    
+//     const userWithoutPassword = user.toJSON();
+//     console.log(userWithoutPassword)
+//     delete userWithoutPassword.password;
+    
+//     return res.json(userWithoutPassword);
+//     }catch(e){
+//         return res.status(500).json({msg : "Internal server error"})
+//     }
+// }
+// exports.login = async (req ,res)=>{
+//     try{
+//         const email = req.body.email;
+//         const password = req.body.password;
+//         const user = await User.findOne({where : {email : email}})
+
+
+//         if(user === null){
+
+//             return res.status(401).json({success : false , msg : "wrong credentials"})
+//         }
+
+//         const result = await bcrypt.compare(password ,user.password)
+//         if(result){
+//             return res.json({success : true})
+//         }else{
+//             return res.status(401).json({success : false , msg : "wrong credentials"})
+//         }
+
+
+//     }catch(e){
+//         return res.status(500).json({msg : "Internal server error"})
+
+//     }
+// }
+
+// //module.exports = router;
---
+++ expensetracker/js/login.js
@@ -0,0 +1,33 @@
+
+
+document.getElementById('login').addEventListener('submit' , loginUser)
+
+const axiosInstance = axios.create({
+    baseURL : "http://localhost:4000/user"
+})
+
+
+async function loginUser(e){
+    e.preventDefault()
+    
+    const data = {
+        email : e.target.email.value,
+        password : e.target.password.value
+    }
+    console.log(data)
+
+    try{
+        const result = await axiosInstance.post('/login' , data)
+        console.log(result)
+        if(result.data.success){
+            alert("login succesfully")
+            localStorage.setItem('token' , result.data.token)
+            window.location ="/"
+        }
+    }catch(e){
+        console.log(e)
+        alert(e.response.data.msg)
+    }
+
+}
+
